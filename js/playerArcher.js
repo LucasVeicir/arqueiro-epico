4bf758e3f065933c9730f355562388b8604709f0
Lucas Veicir <nikliklucas@gmail.com>
2026-07-16 08:45:20 -0300

    player

---
+++ js/playerArcher.js
@@ -0,0 +1,36 @@
+class PlayerArcher{
+    constructor(x,y,w,h){
+        let options = {
+            isStatic: true,
+        }
+
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w;
+        this.h = h;
+        this.collapse = false;
+        this.image = loadImage("./assets/playerArcher.png");
+        World.add(world,this.body);
+        Matter.Body.setAngle(this.body,-PI/2);
+    }
+
+    display(){
+        let pos = this.body.position;
+        let angle = this.body.angle;
+        if(keyIsDown(DOWN_ARROW) && angle<-0.8){
+            angle += 0.02;
+            Matter.Body.setAngle(this.body,angle);
+        }
+
+        if(keyIsDown(UP_ARROW) && angle > -2.2){
+            angle -= 0.02;
+            Matter.Body.setAngle(this.body,angle);
+        }
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.w,this.h);
+        pop();
+    }
+}
--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ let engine;
 let world;
 let canvas;
 let bgImg;
+let playerBase;
+let player;
+let playerArcher;
 
 function preload(){
     bgImg = loadImage('assets/background.png')
@@ -16,8 +19,14 @@ function setup(){
     canvas = createCanvas(windowWidth, windowHeight);
     engine = Engine.create();
     world = engine.world;
+    playerBase = new PlayerBase(300,500,180,150);
+    player = new Player(285,playerBase.body.position.y-153,50,180);
+    playerArcher = new PlayerArcher(340,playerBase.body.position.y-180,120,120);
 }
 
 function draw(){
     background(bgImg);
+    playerBase.display();
+    player.display();
+    playerArcher.display();
 }
