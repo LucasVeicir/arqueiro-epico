c40304054ea574b3aa07562358c93c62a0654479
Lucas Veicir <nikliklucas@gmail.com>
2026-08-05 08:45:14 -0300

    adição dos alvos do jogo

--- sketch.js
+++ sketch.js
@@ -10,6 +10,11 @@ let bgImg;
 let playerBase;
 let player;
 let playerArcher;
+let board1,board2;
+let playerArrow=[];
+let numberOfArows=10;
+let score=0;
+let gameState="play";
 
 function preload(){
     bgImg = loadImage('assets/background.png')
@@ -22,6 +27,8 @@ function setup(){
     playerBase = new PlayerBase(300,500,180,150);
     player = new Player(285,playerBase.body.position.y-153,50,180);
     playerArcher = new PlayerArcher(340,playerBase.body.position.y-180,120,120);
+    board1 = new Board(width-300,330,50,200);
+    board2 = new Board(width-550,height-300,50,200)
 }
 
 function draw(){
@@ -29,4 +36,11 @@ function draw(){
     playerBase.display();
     player.display();
     playerArcher.display();
+    board1.display();
+    board2.display();
+
+    fill("#FFF");
+    textAlign("center");
+    textSize(40);
+    text("arqueiro epico",width/2,100);
 }
