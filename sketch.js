9c050c0d14bb9605cbc3ae0acebb388f33e3213f
Lucas Veicir <nikliklucas@gmail.com>
2026-08-06 08:44:46 -0300

    adição gameOver e resetGame

--- sketch.js
+++ sketch.js
@@ -43,4 +43,46 @@ function draw(){
     textAlign("center");
     textSize(40);
     text("arqueiro epico",width/2,100);
+    textSize(30);
+    text("flechas restantes: "+numberOfArows,200,100);
+    text("pontuação: "+score,width-200,100);
+    if(numberOfArows == 0 && gameState === "play"){
+        gameState = "end";
+        gameOver();
+    }
+}
+
+function gameOver(){
+    swal(
+        {
+            title: "fim de jogo",
+            text : "obrigado por jogar. Sua pontuação: "+ score,
+            icon: "info",
+            content:{
+                element:"img",
+                attributes:{
+                    src:"https://raw.githubusercontent.com/vishalgaddam873/PiratesInvision/main/assets/board.png",
+                    width:150
+                }
+            },
+            button:"jogar novamente"
+        }
+    ).them(()=>{
+        resetGame();
+    })
+}
+
+function resetGame(){
+    playerArrow = [];
+    numberOfArows = 10;
+    score = 0;
+    engine = Engine.create();
+    world = engine.world;
+    world.gravity.y = 0.3;
+    playerBase = new PlayerBase(300,500,180,150);
+    player = new Player(285,playerBase.body.position.y-153,50,180);
+    playerArcher = new PlayerArcher(340,playerBase.body.position.y-180,120,120);
+    board1 = new Board(width-300,330,50,200);
+    board2 = new Board(width-550,height-300,50,200);
+    gameState = "play";
 }
